1ee6120331e5f6769f075fb281de4a2605fcbf11
Glenn Langdon <glennlangdon@gmail.com>
2017-02-04 16:46:10 -0600

    Set production ENV vars for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'glennlangdon@gmail.com',
-    password:             'Llfbfh0084',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
