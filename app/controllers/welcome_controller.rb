cac8f80afa09755beb2be35bb0854ef8ca0d71a2
Glenn Langdon <glennlangdon@gmail.com>
2017-02-06 21:02:59 -0600

    Cleaned spacing in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,15 +8,15 @@ class WelcomeController < ApplicationController
     @contact = Welcome.new(params[:welcome])
     @contact.request = request
     respond_to do |format|
-    if @contact.deliver
-      # re-initialize Welcome object for cleared form
-      @contact = Welcome.new
-      format.html { render 'index'}
-      format.js   { flash.now[:notice] = @message = "Thank you for your message. I'll get back to you soon!" }
-    else
-      format.html { render 'index' }
-      format.js   { flash.now[:error] = @message = "Message did not send." }
+      if @contact.deliver
+        # re-initialize Welcome object for cleared form
+        @contact = Welcome.new
+        format.html { render 'index'}
+        format.js   { flash.now[:notice] = @message = "Thank you for your message. I'll get back to you soon!" }
+      else
+        format.html { render 'index' }
+        format.js   { flash.now[:error] = @message = "Message did not send." }
+      end
     end
   end
-end
 end
